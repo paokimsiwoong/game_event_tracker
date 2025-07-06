2161827c131cc9acd0329ae916c8afe8c881c16f
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-07-07 08:45:38 +0900

    1: update README.md - add a simple description about the project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # game_event_tracker
+
+## 게임 이벤트 일정 캘린더 입력 프로젝트
+---
+### 유저가 게임 이벤트 공지를 제공하는 url (ex: https://sv-news.pokemon.co.jp/ko/list)을 입력하면 그 내용들을 긁어와서 일정이 적혀 있는 이벤트들을 구글 캘린더에 저장합니다.
--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,1 +1,18 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLineParse(t *testing.T) {
+	err := temp()
+	require.NoError(t, err)
+	assert.Equal(t, nil, err)
+}
+
+func temp() error {
+	return nil
+}
