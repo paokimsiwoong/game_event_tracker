61c26268265cee660762cb8c723c97e1cfa44a10
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-07-14 08:12:46 +0900

    10: update internal/crawler/cralwer_test.go - add a test for crawler.go

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## 게임 이벤트 일정 캘린더 입력 프로젝트
 ---
-### 유저가 게임 이벤트 공지를 제공하는 url (ex:https://sv-news.pokemon.co.jp/ko/list) 을 입력하면 그 내용들을 긁어와서 일정이 적혀 있는 이벤트들을 구글 캘린더에 저장합니다.
+### 유저가 게임 이벤트 공지를 제공하는 url (ex:https://sv-news.pokemon.co.jp/ko/list => https://sv-news.pokemon.co.jp/ko/json/list.json) 을 입력하면 그 내용들을 긁어와서 일정이 적혀 있는 이벤트들을 구글 캘린더에 저장합니다.
 
 ```
 game-event-calendar/
--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
@@ -35,6 +36,8 @@ type PokeSVResult struct {
 
 const pokeURL = "https://sv-news.pokemon.co.jp/ko/"
 
+// const pokeURL = "https://sv-news.pokemon.co.jp/ko/json/list.json"
+
 func PokeCrawl(url string, duration int) ([]PokeSVResult, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -59,15 +62,37 @@ func PokeCrawl(url string, duration int) ([]PokeSVResult, error) {
 	checkPoint := now.AddDate(0, 0, -duration)
 	ts := checkPoint.Unix()
 
+	// 시간순 정렬 확실하게 하기위해 미리 정렬 실행
+	// sort.Slice(pokeSVJSON.Data, func(i, j int) bool {
+	// 	// return pokeSVJSON.Data[i].StAt < pokeSVJSON.Data[j].StAt
+	// 	ith, _ := strconv.ParseInt(pokeSVJSON.Data[i].StAt, 10, 64)
+	// 	jth, _ := strconv.ParseInt(pokeSVJSON.Data[j].StAt, 10, 64)
+	//  @@@ 변환 두번 할 필요 없도록 stAt의 타입이 int 64인 새 struct를 만들어
+	//  @@@ stAt의 타입 변환 후 저장한 것을 가지고 정렬?
+	// 	return ith < jth
+	// })
+
 	for _, data := range pokeSVJSON.Data {
+
 		stAtUnix, err := strconv.ParseInt(data.StAt, 10, 64)
 		if err != nil {
 			return nil, fmt.Errorf("error calling strconv.ParseInt: %w", err)
 		}
+
 		// 지정한 시점보다 과거의 이벤트는 무시
 		if stAtUnix < ts {
-			continue
+			// continue
+			// 일단 정렬이 되어있으므로 break로 변경?
+			// 확실하게 정렬 추가?
+			break
 		}
+
+		// 임시로 data 타이틀과 날짜 확인
+		t := time.Unix(stAtUnix, 0)
+		// kst := t.In(time.FixedZone("KST", 9*60*60))
+		// time.FixedZone은 지정한 이름과 UTC 기준 오프셋을 입력하여 만드는 커스텀 시간대
+		kst := t.Local()
+		log.Printf("data title: %v\nposted at: %v\n", data.Title, kst)
 
 		dataResp, err := http.Get(pokeURL + data.Link)
 		if err != nil {
