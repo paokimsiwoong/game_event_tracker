f09a933b09e70372a5057fa87022a3af6d359cfa
paokimsiwoong <pao.kim.si.woong@gmail.com>
2025-08-23 07:52:36 +0900

    51: update internal/commands/events.go - change the default behavior of events command

--- internal/commands/events.go
+++ internal/commands/events.go
@@ -9,60 +9,76 @@ import (
 
 // posts 테이블의 post 데이터들을 정리해 events 테이블에 저장하는 핸들러
 func HandlerEvents(s *State, cmd Command) error {
-	count := 0
+	// @@@ events 테이블에 데이터 등록하는 부분 제외하는 옵션 추가
+	if len(cmd.Args) != 0 {
+		switch cmd.Args[0] {
+		case "-r":
+			fallthrough
+		case "-register":
+			fallthrough
+		case "register":
+			fallthrough
+		case "r":
+			count := 0
 
-	posts, err := s.PtrDB.GetPostsAndSites(context.Background())
-	if err != nil {
-		return fmt.Errorf("error getting posts table: %w", err)
+			posts, err := s.PtrDB.GetPostsAndSites(context.Background())
+			if err != nil {
+				return fmt.Errorf("error getting posts table: %w", err)
+			}
+
+			fmt.Println("--------------------------------------------------")
+			fmt.Println("--------------------------------------------------")
+			fmt.Printf("%d posts in the table\n", len(posts))
+			fmt.Println("--------------------------------------------------")
+			fmt.Println("--------------------------------------------------")
+
+			if len(posts) == 0 {
+				fmt.Println("0 event registered")
+				fmt.Println("--------------------------------------------------")
+				fmt.Println("--------------------------------------------------")
+				return nil
+			}
+
+			for _, post := range posts {
+				// 이미 등록된 post면 continue
+				if post.Registered {
+					fmt.Printf("The post %s(url:%s) has already been registered to events table in db\n", post.Name, post.PostUrl)
+					continue
+				}
+
+				event, err := s.PtrDB.CreateEvent(context.Background(), database.CreateEventParams{
+					Tag:      post.Tag,
+					TagText:  post.TagText,
+					StartsAt: post.StartsAt,
+					EndsAt:   post.EndsAt,
+					Column5:  post.Name,
+					Column6:  post.PostedAt,
+					Column7:  post.PostUrl,
+					Column8:  post.ID,
+					SiteID:   post.SiteID,
+				})
+				if err != nil {
+					return fmt.Errorf("error creating an event: %w", err)
+				}
+
+				if len(event.Names) == 1 {
+					count++
+				}
+
+				// 등록 완료한 post는 registered 칼럼 값 true로 변경
+				err = s.PtrDB.SetPostRegisteredTrue(context.Background(), post.ID)
+				if err != nil {
+					return fmt.Errorf("error updating a post: %w", err)
+				}
+			}
+
+			fmt.Printf("%d events registered\n", count)
+
+		default:
+			fmt.Println("Invalid argument. Will list events without registering")
+		}
 	}
 
-	fmt.Println("--------------------------------------------------")
-	fmt.Println("--------------------------------------------------")
-	fmt.Printf("%d posts in the table\n", len(posts))
-	fmt.Println("--------------------------------------------------")
-	fmt.Println("--------------------------------------------------")
-
-	if len(posts) == 0 {
-		fmt.Println("0 event registered")
-		fmt.Println("--------------------------------------------------")
-		fmt.Println("--------------------------------------------------")
-		return nil
-	}
-
-	for _, post := range posts {
-		// 이미 등록된 post면 continue
-		if post.Registered {
-			fmt.Printf("The post %s(url:%s) has already been registered to events table in db\n", post.Name, post.PostUrl)
-			continue
-		}
-
-		event, err := s.PtrDB.CreateEvent(context.Background(), database.CreateEventParams{
-			Tag:      post.Tag,
-			TagText:  post.TagText,
-			StartsAt: post.StartsAt,
-			EndsAt:   post.EndsAt,
-			Column5:  post.Name,
-			Column6:  post.PostedAt,
-			Column7:  post.PostUrl,
-			Column8:  post.ID,
-			SiteID:   post.SiteID,
-		})
-		if err != nil {
-			return fmt.Errorf("error creating an event: %w", err)
-		}
-
-		if len(event.Names) == 1 {
-			count++
-		}
-
-		// 등록 완료한 post는 registered 칼럼 값 true로 변경
-		err = s.PtrDB.SetPostRegisteredTrue(context.Background(), post.ID)
-		if err != nil {
-			return fmt.Errorf("error updating a post: %w", err)
-		}
-	}
-
-	fmt.Printf("%d events registered\n", count)
 	fmt.Println("--------------------------------------------------")
 	fmt.Println("--------------------------------------------------")
 
